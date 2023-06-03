6e95b72db0ed9510462f21e95c2b00ec07171f71
Matveev Maksim <thematveev21@gmail.com>
2023-06-03 04:31:10 +0300

    bot

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ bot = TeleBot('5806041163:AAG3F_eOFBztryotOQG5C9_KO0Y7t7otsN8')
 def position_handler(message):
     data = Cords.select().order_by(Cords.time.desc()).get()
     bot.send_message(message.chat.id, f"http://maps.google.com/maps?q=+{data.lat},+{data.lon}")
+    bot.send_message(message.chat.id, data.time)
 
 
 bot.polling()
